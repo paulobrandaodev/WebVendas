c49af8772398fb17ba028848da76cc2df05cb46f
Paulo Brandão <paulobrandaoofficial@gmail.com>
2022-04-12 15:04:40 -0300

    Aula13

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../Assets/Img/logo.png';
-import { Botao } from '../../Components/StyledComponents/Botao';
+import Load from '../../Services/loginGoogle';
 import './index.css';
 
 
 export default class Home extends React.Component {
-    
+
+    componentDidMount() {
+        Load();
+    }   
+
     render() {
         return(
             <div className='main_home'>
                 <img src={logo} alt="Logo" />
                 <h1>{this.props.titulo}</h1>
                 <Link to='/MeusClientes' className='btn btn_gray'>Entrar</Link>
-                <Botao tamanho="200">
-                    <Link to='/MeusClientes'>Sair</Link>
-                </Botao>
+                <div id="buttonDiv"></div> 
             </div>
         );
     }
---
+++ src/Services/loginGoogle.js
@@ -0,0 +1,33 @@
+export function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var token = parseJwt(response.credential);    
+    localStorage.setItem('tokenGoogle', JSON.stringify(token));
+    window.location.replace('http://localhost:3000/MeusClientes');
+}
+
+export function Load(){
+    window.onload = function () {
+        window.google.accounts.id.initialize({
+          client_id: "58617903465-2ls6ppmjka8skkvesie8kgs7msr78ohm.apps.googleusercontent.com",
+          callback: handleCredentialResponse
+        });
+        window.google.accounts.id.renderButton(
+          document.getElementById("buttonDiv"),
+          { theme: "outline", size: "large" }  // customization attributes
+        );
+        window.google.accounts.id.prompt(); // also display the One Tap dialog
+    }
+
+}
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
+export default Load;
